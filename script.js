fee54a93773923967b5369d92043dce371361773
Matt Carver <mattcarver.wd@gmail.com>
2021-07-13 16:55:52 -0500

    styling pictures

--- script.js
+++ script.js
@@ -8,11 +8,39 @@ const search = document.getElementById('search'),
 //Function, search meal and fetch from API
 function searchMeal(e) {
     e.preventDefault();
+
 //Clear single meal element
     single_mealEl.innerHTML = '';
 
 //Get search term
-const term = search.nodeValue;
+const term = search.value;
+//Check for empty
+if (term.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`
+
+        if(data.meals === null) {
+            resultHeading.innerHTML = `<p>There are no search results.  Try again!</p>`
+        } else {
+            mealsEl.innerHTML = data.meals.map(
+                meal => `
+                <div class="meal">
+                <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+                <div class="meal-info" data-mealID="${meal.idMeal}">
+                <h3>${meal.strMeal}</h3>
+                </div>
+                </div>
+                `)
+        }
+    });
+    //Clear search text
+    search.value = '';
+} else {
+    alert('Please enter a search term!');
+}
 
 console.log(term);
 }
